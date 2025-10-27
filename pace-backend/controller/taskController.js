0c31db088a9f4ce07b2244399bf172f2dcb8bc8b
Abhin <abhin1511203@gmail.com>
2025-10-27 21:44:38 +0530

    feat:Implemented CRUD on task management

---
+++ pace-backend/controller/taskController.js
@@ -0,0 +1,114 @@
+import asyncHandler from "../middlewares/asyncHandler.js";
+import Task from "../models/taskModel.js";
+import { HTTP_STATUS } from "../utils/httpStatus.js";
+
+export const addTask = asyncHandler(async (req, res) => {
+  const { activity, description } = req.body;
+  const assignedBy = req.user.id;
+
+  if (!activity) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Activity is required" });
+  }
+  if (!description) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Description is required" });
+  }
+
+  const newTask = {
+    activity,
+    description,
+    assignedBy,
+  };
+
+  const task = await Task.create(newTask);
+
+  res
+    .status(HTTP_STATUS.CREATED)
+    .json({ message: "task created successfully", task });
+});
+
+export const getTask = asyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  if (!taskId) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Task ID is required" });
+  }
+  const task = await Task.findById(taskId);
+
+  if (!task) {
+    return res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Task not found" });
+  }
+  res.status(HTTP_STATUS.OK).json({ message: "task found", task });
+});
+
+export const getAllTasks = asyncHandler(async (req, res) => {
+  const tasks = await Task.find();
+  if (!tasks) {
+    return res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Tasks not found" });
+  }
+  res.status(HTTP_STATUS.OK).json({ message: "tasks found", tasks });
+});
+
+export const updateTask = asyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  const { activity, description } = req.body;
+  const assignedBy = req.user.id;
+
+  if (!activity) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Activity is required" });
+  }
+  if (!description) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Description is required" });
+  }
+
+  const newData = {
+    activity,
+    description,
+    assignedBy,
+  };
+
+  const task = await Task.findByIdAndUpdate(taskId, newData, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!task) {
+    return res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Task not found" });
+  }
+
+  res
+    .status(HTTP_STATUS.OK)
+    .json({ message: "Task updated successfully", task });
+});
+
+export const deleteTask = asyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  if (!taskId) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: "Task ID is required" });
+  }
+  const task = await Task.findByIdAndDelete(taskId);
+  if (!task) {
+    return res
+      .status(HTTP_STATUS.NOT_FOUND)
+      .json({ message: "Task not found" });
+  }
+
+  // res.status(HTTP_STATUS.NO_CONTENT).send();
+  res.status(HTTP_STATUS.OK).json({ message: "task deleted successfully" });
+});
---
+++ pace-backend/models/taskModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    activity: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    assignedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
